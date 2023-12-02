f9fb702ed5126a1bdc8a8589581dec6cdc61c0c7
CristianCiubancan <balooprogames@gmail.com>
2023-12-02 11:09:46 -0800

    migrated-to-.net-7-and-added-some-canyon-code

---
+++ src/Comet.Shared/Rest/Models/HttpClientRequestLog.cs
@@ -0,0 +1,9 @@
+﻿namespace Comet.Shared.Rest.Models
+{
+    public sealed class HttpClientRequestLog
+    {
+        public string Method { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ src/Comet.Shared/Rest/Models/HttpClientResponseLog.cs
@@ -0,0 +1,11 @@
+﻿namespace Comet.Shared.Rest.Models
+{
+    public sealed class HttpClientResponseLog
+    {
+
+        public int HttpStatus { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public string Body { get; set; }
+
+    }
+}
